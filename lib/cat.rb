deddfac5c06903d5ee4a1271d99a57ea7cb4238d
Willis Aybar <willisaybar@gmail.com>
2020-02-27 23:07:46 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
-  # code goes here
+  attr_accessor :name, :owner
+
+  def initilize(name, owner)
+    @name =  name
+    @owner = owner
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
-  # code goes here
+  attr_accessor :name, :owner
+
+  def initilize(name, owner)
+    @name =  name
+    @owner = owner
+  end
+  
 end
